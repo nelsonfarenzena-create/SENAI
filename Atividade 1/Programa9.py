39c1ecd9dc48bedbff52d5d804d63f1c8fd226d8
nelsonfarenzena-create <nelson_farenzena@estudante.sesisenai.org.br>
2026-03-20 20:19:37 +0000

    uptade

---
+++ Atividade 1/Programa9.py
@@ -0,0 +1,4 @@
+valor_gasolina = float(input("Valor Da Gasolina: "))
+quantidade_litros = float(input("Quantidade De Litros: "))
+valor_gasto = valor_gasolina * quantidade_litros
+print("O Valor Gasto Será: ",valor_gasto)
--- Atividade 1/Progrma8.py
+++ Atividade 1/Progrma8.py
@@ -1,9 +1,10 @@
-nome = (input("Insira Seu Nome: "))
-idade = (input("Insira sua Idade: "))
-dias_de_vida = idade * 365
-if idade < 0
-    print("Não se pode inserir uma idade menor que 0.")
-else idade > 120
-    print("Não se pode inserir uma idade maior que 120.")
+nome = input("Insira seu nome: ")
+idade = int(input("Insira sua idade: "))
 
-print("você já viveu: ",dias_de_vida "de sua vida")
+if idade < 0:
+    print("Não se pode inserir uma idade menor que 0.")
+elif idade > 120:
+    print("Não se pode inserir uma idade maior que 120.")
+else:
+    dias_de_vida = idade * 365
+    print("Você já viveu:", dias_de_vida, "dias de sua vida")
