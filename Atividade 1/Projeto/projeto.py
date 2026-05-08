2679aa20d2bd4107cd5a1ad3f0bb33ddd8ce7268
nelsonfarenzena-create <nelson_farenzena@estudante.sesisenai.org.br>
2026-05-08 20:19:24 +0000

    uptade

---
+++ Atividade 1/Projeto/projeto.py
@@ -0,0 +1,8 @@
+volume_atual = 50.0
+perda_por_hora = 2
+tempo_decorrido = 1
+
+while volume_atual > 5: 
+    print(f"Hora {tempo_decorrido}")
+    print(f"Combustível restante {volume_atual} litros" )
+    volume_atual -= perda_por_horatempo_decorrido += 1
